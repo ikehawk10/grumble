199f21f6fcda24237ebd1f949a3eb025d5821032
Di Gandy <digandy@Dis-MacBook-Pro.local>
2017-04-06 17:48:48 -0500

    please work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,29 @@
 
 // // var googleMapsKey = "&key=AIzaSyBfQu6oJU6bPRuUyHqpk8HStkK76-cHBN0";
 // // var weatherKey = " 88fa17d19b77bfc5";
-	
+
 // // var origin = "origin=Disneyland&";
 // // var destination ="destination=Universal+Studios+Hollywood4";
 // // var queryURL = "https://maps.googleapis.com/maps/api/directions/json?&"  + origin + destination +  googleMapsKey;
 var userZIP;
 var mileRadius;
+var userZiplatitude;
+var userZiplongitude;
+
+function getCoords() {
+
+    var geocoder = new google.maps.Geocoder();
+    var address = userZIP;
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            userZiplatitude = results[0].geometry.location.lat();
+            userZiplongitude = results[0].geometry.location.lng();
+            console.log("Latitude: " + userZiplatitude + "\nLongitude: " + userZiplongitude);
+        } else {
+            alert("Request failed.")
+        }
+    });
+};
 
 
 
@@ -30,98 +47,103 @@ var mileRadius;
 // 	$("#location").toggle("slide");
 // });
 
-$(document).ready(function(){
-	$("#location").hide();
-	$("#cuisines").hide();
-	$("#pricePoint").hide();
-	$("#results").hide();
-	$(".food-photos").hide();
+$(document).ready(function() {
+    $("#location").hide();
+    $("#cuisines").hide();
+    $("#pricePoint").hide();
+    $("#results").hide();
+    $(".food-photos").hide();
 
-	// $("#slideshow").hide();
+    // $("#slideshow").hide();
 });
 
-$("#start").on("click", function(event){
-	event.preventDefault();
-	$("#startScreen").toggle("slide");
-	$("#location").toggle("slide");
-	$("#location").show();
-	$("#slideshow").hide();
+$("#start").on("click", function(event) {
+    event.preventDefault();
+    $("#startScreen").toggle("slide");
+    $("#location").toggle("slide");
+    $("#location").show();
+    $("#slideshow").hide();
 
 });
 
 $(document).ready(function() {
-	$('#slideshow').cycle({
-	fx: 'fade',
-	pager: '#smallnav',
-	pause:   5,
-	speed: 2000,
-	timeout:  3500
-	});
+    $('#slideshow').cycle({
+        fx: 'fade',
+        pager: '#smallnav',
+        pause: 5,
+        speed: 2000,
+        timeout: 3500
+    });
 
-	console.log("slideshow");
+    console.log("slideshow");
 });
 
 
 
-$("#distance-submit").on("click", function(event){
-	event.preventDefault();
-	userZIP = $("#zip-input").val().trim();
-	mileRadius = $("#distance-select").val();
-	$("#location").toggle("slide");
-	$("#cuisines").toggle("slide");
-	$("#cuisines").show();
+$("#distance-submit").on("click", function(event) {
+    event.preventDefault();
+    userZIP = $("#zip-input").val().trim();
+    mileRadius = $("#distance-select").val();
+    $("#location").toggle("slide");
+    $("#cuisines").toggle("slide");
+    $("#cuisines").show();
 
-
-	// $("#location").toggle("slide");
-
-
-	console.log(userZIP);
-	console.log(mileRadius);
-
- 
+    getCoords();
 
 
 
-var queryWeather = "http://api.wunderground.com/api/88fa17d19b77bfc5/conditions/q/" + userZIP + ".json";
 
-// $.ajax({
-// 	url: queryWeather,
-// 	method: "GET",
-	
+    // $("#location").toggle("slide");
 
-// }).done(function(weather) {
-// 	console.log(queryWeather);
-// 	console.log(weather);
-// 	});
+
+
+    console.log(userZIP);
+    console.log(mileRadius);
+
+
+
+
+
+    var queryWeather = "http://api.wunderground.com/api/88fa17d19b77bfc5/conditions/q/" + userZIP + ".json";
+
+    // $.ajax({
+    // 	url: queryWeather,
+    // 	method: "GET",
+
+
+    // }).done(function(weather) {
+    // 	console.log(queryWeather);
+    // 	console.log(weather);
+    // 	});
 
 
 });
 
-$("#cuisines-submit").on("click", function(event){
-	event.preventDefault();
-	$("#cuisines").toggle("slide");
-	$("#pricePoint").toggle("slide");
-	$("#pricePoint").show();
+$("#cuisines-submit").on("click", function(event) {
+    event.preventDefault();
+    $("#cuisines").toggle("slide");
+    $("#pricePoint").toggle("slide");
+    $("#pricePoint").show();
 });
 
-$("#pricePoint-submit").on("click", function(event){
-	event.preventDefault();
-	$("#pricePoint").toggle("slide");
-	$("#results").toggle("slide");
-	$("#results").show();
+$("#pricePoint-submit").on("click", function(event) {
+    event.preventDefault();
+    $("#pricePoint").toggle("slide");
+    $("#results").toggle("slide");
+    $("#results").show();
 });
 
 
-$("#flip1").on("click", function(){
-	$(this).toggleClass("flip-container");
+$("#flip1").on("click", function() {
+    $(this).toggleClass("flip-container");
 });
 
-$("#flip2").on("click", function(){
-	$(this).toggleClass("flip-container");
+$("#flip2").on("click", function() {
+    $(this).toggleClass("flip-container");
 });
 
-$("#flip3").on("click", function(){
-	$(this).toggleClass("flip-container");
+$("#flip3").on("click", function() {
+    $(this).toggleClass("flip-container");
 });
 
 
